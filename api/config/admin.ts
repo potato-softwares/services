f9e6fc0af1e6462fdf8d9fa2872ff8592be22c16
chientrm <chientrm@gmail.com>
2022-11-12 07:35:56 +0000

    init

--- api/config/admin.ts
+++ api/config/admin.ts
@@ -1,8 +1,23 @@
-export default ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-});
+export default ({ env }) => {
+  const config = {
+    auth: { secret: env("ADMIN_JWT_SECRET") },
+    apiToken: { salt: env("API_TOKEN_SALT") },
+  };
+  if (process.env.GITPOD_WORKSPACE_URL) {
+    return {
+      ...config,
+      webpack: (config, webpack) => {
+        config.devServer = {
+          host: "0.0.0.0",
+          public: process.env.GITPOD_WORKSPACE_URL.replace(
+            "https://",
+            "https://1337-"
+          ),
+          disableHostCheck: true,
+        };
+        return config;
+      },
+    };
+  }
+  return config;
+};
--- api/config/server.ts
+++ api/config/server.ts
@@ -1,7 +1,18 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  const port = 1337,
+    config = {
+      host: env("HOST", "0.0.0.0"),
+      port: env.int("PORT", port),
+      app: {
+        keys: env.array("APP_KEYS"),
+      },
+    };
+  if (process.env.GITPOD_WORKSPACE_URL) {
+    const url = process.env.GITPOD_WORKSPACE_URL.replace(
+      "https://",
+      `https://${port}-`
+    );
+    return { ...config, url, admin: { url: `${url}/admin` } };
+  }
+  return config;
+};
